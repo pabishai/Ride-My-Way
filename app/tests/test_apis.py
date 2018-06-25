416e1dbb70ff080837aa9cddb6484a0d15ce06eb
PAbishai <phillipabishai@gmail.com>
2018-06-25 19:32:03 +0300

    Added the test folder

---
+++ app/tests/test_apis.py
@@ -0,0 +1,73 @@
+import pytest
+import json
+from resources.apis import app
+
+@pytest.fixture 
+def client():
+    test_client = app.test_client() # setup a test client to run the api
+    return test_client
+
+@pytest.fixture
+def sample_ride():
+    # sample_data added to api_rides as first dictionary in the list
+    test_rides = [
+        {
+            "id":1,
+            "driver_id":2,#id of user who is driver
+            "location_from":"Ruaka",   
+            "destination":"Nairobi CBD",
+            "leaving":"12:00 noon",
+            "full":False,
+            "arrived":False
+        },
+        {
+            "id":2,
+            "driver_id":3,
+            "location_from":"Thika Road",   
+            "destination":"Nairobi CBD",
+            "leaving":"10:00 am",
+            "full":False,
+            "arrived":False
+        }
+    ]
+
+    return test_rides
+
+"""add a test ride first to allow for the other tests
+"""
+
+def test_api_addRide(client,sample_ride):
+    response = client.post('/api/v1/rides',data = json.dumps(sample_ride), content_type = 'application/json')
+    result = json.loads(response.data)
+    #test if response code is 201 CREATED
+    assert response.status_code == 201
+    #test if data matches
+    data = result["ride"]
+    assert data["driver_id"] == sample_ride["driver_id"]
+    assert data["location"] == sample_ride["location"]
+    assert data["destination"] == sample_ride["destination"]
+    assert data["leaving"] == sample_ride["leaving"]
+
+def test_api_rides(client,sample_ride):
+    response = client.get('/api/v1/rides')
+    result = json.loads(response.data)
+    # test if response code is OK
+    assert response.status_code == 200
+    # test if values returned match
+    assert result["rides"] == sample_ride
+    # test if the number of rides returned is the same
+    assert len(result["rides"]) == 2
+
+def test_api_singleRides(client,sample_ride):
+    ride_id = 2 #set the ride_id
+    response = client.get("/api/v1/rides/"+str(ride_id))
+    result = json.loads(response.data)
+    #test that the respose code is 200 ok
+    assert response.status_code == 200
+    #test that it only retrieves one item
+    assert len(result["ride"]) ==  1
+    #test that the item received matches
+    assert result["ride"][0] == sample_ride[1]
+
+
+

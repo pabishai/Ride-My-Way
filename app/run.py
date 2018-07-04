dfc3e403ebe1ba0f327fdb349e1c08d8feba2982
PAbishai <phillipabishai@gmail.com>
2018-07-05 00:11:44 +0300

    Added namespaces and registered models
    used namespaces for cleaner API documentation
    registered the models for the API documentation here

--- app/run.py
+++ app/run.py
@@ -1,17 +1,24 @@
 from flask import Flask, Blueprint
-from flask_restful import Api
+from flask_restplus import Api, fields
 from flask_jwt_extended import JWTManager
 
 from . import views, models, resources
 
-
 app = Flask(__name__)
-api = Api(app)
 
 jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'siri-ingine'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+api = Api(app, authorizations=authorizations, security='Bearer apikey')
 
 
 @jwt.token_in_blacklist_loader
@@ -21,14 +28,35 @@ def check_token(decrypted_token):
 
 
 api.add_resource(views.Hello, '/')
-api.add_resource(resources.RegisterUser, '/api/v2/auth/signup')
-api.add_resource(resources.LoginUser, '/api/v2/auth/login')
-api.add_resource(resources.RideResource, '/api/v2/users/rides', '/api/v2/rides')
-api.add_resource(resources.RideDetails, '/api/v2/rides/<ride_id>')
-api.add_resource(
-    resources.Requests, 
-    '/api/v2/users/rides/<ride_id>/requests', 
-    '/api/v2/rides/<ride_id>/requests', 
-    '/api/v2/users/rides/<ride_id>/requests/<request_id>'
-    )
 
+""" Register user
+    -----
+    Add namespace and model for documentation and resource for the path for the documentation
+"""
+api.models[resources.user_model.name] = resources.user_model
+api.models[resources.login_model.name] = resources.login_model
+ns_user = api.namespace('user apis', description='Register and Login Users APIs', path='/api/v2/auth')
+ns_user.add_resource(resources.RegisterUser, '/signup')
+ns_user.add_resource(resources.LoginUser, '/login')
+
+""" Add Ride, Show all Rides, View Ride Details
+    -----
+    Add models and namespace for documentation and resource for the path for the documentation
+"""
+api.models[resources.ride_model.name] = resources.ride_model
+api.models[resources.ride_schema_model.name] = resources.ride_schema_model
+api.models[resources.ride_details_schema_model.name] = resources.ride_details_schema_model
+ns_rides = api.namespace('rides apis', description='Add ride, View Rides, View Ride Details', path='/api/v2')
+ns_rides.add_resource(resources.RidesResource, '/rides', '/rides')
+ns_rides.add_resource(resources.RideDetailsResource, '/rides/<ride_id>')
+
+""" Post a Request To Join a Ride, Show Requests for a Ride, Edit Ride Request
+    -----
+    Add namespace for documentation and resource for the path for the documentation
+"""
+api.models[resources.request_model.name] = resources.request_model
+api.models[resources.request_view_model.name] = resources.request_view_model
+api.models[resources.request_status_model.name] = resources.request_status_model
+ns_requests = api.namespace('request apis', description='Add Ride Request, View a Ride Request, Edit a Ride Request', path='/api/v2')
+ns_requests.add_resource(resources.RequestsResource, '/rides/<rideId>/requests', '/rides/<rideId>/requests') 
+ns_requests.add_resource(resources.PutRequestResource, '/rides/<rideId>/requests/<requestId>')
